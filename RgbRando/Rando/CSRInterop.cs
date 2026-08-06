36363098e21bdc3f0a87c4b33f82352be9c77114
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2026-08-05 22:51:32 -0500

    ConnectionSettingsRando interop and log settings

---
+++ RgbRando/Rando/CSRInterop.cs
@@ -0,0 +1,16 @@
+﻿using ConnectionSettingsRando;
+
+namespace RgbRando.Rando
+{
+    internal class CSRInterop
+    {
+        public static void Hook()
+        {
+            CSR.Register(
+                RgbRandoMod.Instance.GetName(),
+                () => RgbRandoMod.GS,
+                s => SettingsRandomizer.CopyTo(s, RgbRandoMod.GS)
+            );
+        }
+    }
+}
